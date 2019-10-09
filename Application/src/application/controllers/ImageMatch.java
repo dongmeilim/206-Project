8af7cd520ecf1122209db0b6fe912673bb469f51
Mirlington <Mirlington@users.noreply.github.com>
2019-10-10 09:54:55 +1300

    Working on thumbnails for ImageMatch

--- Application/src/application/controllers/ImageMatch.java
+++ Application/src/application/controllers/ImageMatch.java
@@ -1,8 +1,26 @@
 package application.controllers;
 
-import javafx.fxml.FXML;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
+import application.controllers.ImageOrder.DragImageCell;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 
 
 /**
@@ -17,6 +35,9 @@ public class ImageMatch extends Controller {
 	@FXML private Button _home;
 	@FXML private Button _help;
 	@FXML private Button _match;
+	@FXML private ListView _thumbnails;
+	
+	private ObservableList<Image> _observableImages = FXCollections.observableArrayList(); 
 	
 	@FXML 
 	private void handleBack() {
@@ -40,11 +61,58 @@ public class ImageMatch extends Controller {
 	@FXML private void handleMatch() {
 		
 	}
-	
-	//private void setImages() {
+	/*
+	private void setImages(int questionAmount) {
+		ListView<St>
+		 List<File> creations = listDirectory("creations");
+		 for (int i = 0; i < (questionAmount - 1); i++) {
+		  		_imageList = new ListView<String>();
+		_imageList.getItems().addAll(savedImages);
+		try {
+			for (String imageName: savedImages) { //load images from files
+			
+				FileInputStream inputstream;
+				inputstream = new FileInputStream("tmp/images/"+imageName);
+				Image image = new Image(inputstream);
+				_observableImages.add(image);
+			}	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}	
+		_imageList.setCellFactory(param -> new DragImageCell());
+		_imageList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		_imageList.setPrefHeight(_HEIGHT);
+		_content.getChildren().add(_imageList);	
+		 * }
+		 
 		//TODO implement setter
-	//}
+	}*/
 	
+	private class Thumbnail extends ListCell<String> {
+			
+			private final ImageView _imageView = new ImageView();
+			
+			public Thumbnail() {
+				setContentDisplay(ContentDisplay.GRAPHIC_ONLY); 
+				setAlignment(Pos.CENTER);
+	    }
+		
+		@Override
+	    protected void updateItem(String item, boolean empty) { //changes the order
+	        super.updateItem(item, empty);
+	        
+	        if (empty || item == null) {
 	
-	
+	        	setGraphic(null);
+	        	
+	        } else {
+	        	_imageView.setImage(_observableImages.get(getListView().getItems().indexOf(item)));
+	        	_imageView.setFitHeight(50);
+	        	_imageView.setFitWidth(50);
+	        	_imageView.setPreserveRatio(true);
+	        	
+	            setGraphic(_imageView); 
+	        }
+	    }
+	}	
 }
